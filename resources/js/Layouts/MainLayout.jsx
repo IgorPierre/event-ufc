5d019ab6d152c025d4e381a5c56dd03f870921cb
IgorPierre <igor.pierre45@gmail.com>
2025-02-24 17:34:44 -0300

    wip

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/react';
 
 export default function MainLayout({ children }) {
     return (
-        <div className="bg-slate-100 flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0">
+        <div className="bg-slate-120 flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0">
             <div className="flex justify-center items-center flex-col w-full h-screen p-14">
                 {children}
             </div>
