f57d3c42110d39ace14845867e05f6ad2dfd3f48
IgorPierre <igor.pierre45@gmail.com>
2025-02-23 23:16:34 -0300

    feat: create the main layout

---
+++ resources/js/Layouts/MainLayout.jsx
@@ -0,0 +1,10 @@
+import ApplicationLogo from '@/Components/ApplicationLogo';
+import { Link } from '@inertiajs/react';
+
+export default function MainLayout({ children }) {
+    return (
+        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+            {children}
+        </div>
+    );
+}
---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/Layouts/MainLayout.jsx";
+import {Head} from "@inertiajs/react";
+
+export default function Home() {
+    return (
+        <MainLayout>
+            <Head title="Início" />
+
+            olá mundo!
+        </MainLayout>
+    );
+}
