dea8c64fb2b56e44d967889763664878cff79ee1
IgorPierre <igor.pierre45@gmail.com>
2025-02-26 00:22:17 -0300

    wip

--- resources/js/Pages/Profile/Partials/DeleteUserForm.jsx
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.jsx
@@ -53,10 +53,7 @@ export default function DeleteUserForm({ className = '' }) {
                 </h2>
 
                 <p className="mt-1 text-sm text-gray-600">
-                    Once your account is deleted, all of its resources and data
-                    will be permanently deleted. Before deleting your account,
-                    please download any data or information that you wish to
-                    retain.
+                    Atenção! Esta ação não poderá ser revertida.
                 </p>
             </header>
 
